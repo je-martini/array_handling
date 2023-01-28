07e1e94447fd26f61a6a42190de676d9f7d7d653
je-martini <jesusmartini2017@gmail.com>
2023-01-28 09:12:47 -0500

    update

--- server/reduce_reload.js
+++ server/reduce_reload.js
@@ -1,28 +1,52 @@
-const items = [1,3,2,3, 3, 1, 10 ];
+// const items = [1,3,2,3, 3, 1, 10 ];
 
-const answer = items.reduce( (obj, item) =>{
-     if(!obj[item]){
-        obj[item] = 1;
-     } else {
-        obj[item] = obj[item] + 1 
-     }
-     return obj
- }, {});
+// const answer = items.reduce( (obj, item) =>{
+//      if(!obj[item]){
+//         obj[item] = 1;
+//      } else {
+//         obj[item] = obj[item] + 1 
+//      }
+//      return obj
+//  }, {});
 
- console.log(answer)
+//  console.log(answer)
 
 
-//output
-// {
-//     1:1,
-//     2:1,
-//     3:2,
-// }
+const arr = [1,2,3,4,5,6,7,8,9,10]
 
-// let obj = {
-    
-// }
+const answer = arr.reduce( (obj, item) =>{
+    if(item <= 5){
+        if(!obj['less_5']){
+            obj['less_5'] = 1
+        } else {
+            obj['less_5'] = obj['less_5'] + 1
+        }
 
-// obj.a = 2
+    }
+    if(item > 5 && item <= 8){
+        if(!obj['middle']){
+            obj['middle'] = 1
+        } else {
+            obj['middle'] = obj['middle'] + 1
+        }
+    }
+    if(item > 8){
+        if(!obj['over_8']){
+            obj['over_8'] = 1
+        } else {
+            obj['over_8'] = obj['over_8'] + 1
+        }
+    }
+    return obj
+}, {});
 
-// console.log(obj.a + 1)
+console.log(answer)
+
+function calcSum(numbers) {
+    let answer = numbers.reduce((total, n) => {
+        return total += n
+      }, 0)
+      return answer
+  }
+
+  console.log(calcSum([1,2,3,4]))
